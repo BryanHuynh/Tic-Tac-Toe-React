a207cef160b5bf971107aa37b716f364ce572316
bryanhuynh <bryan.huynh1@ucalgary.ca>
2021-06-23 20:16:27 -0600

    board now holds square states. toggles between X's and O's

--- src/index.js
+++ src/index.js
@@ -15,22 +15,44 @@ class Square extends React.Component {
         <button 
             className="square" 
             onClick={
-                () => {this.setState({value: 'X'})}
+                () => {this.props.onClick()}
             }
         >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
 }
   
 class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            squares : Array(9).fill(null),
+            xIsNext : true,
+        };
+    }
+
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+        
+    }
     renderSquare(i) {
-        return <Square value={i}/>;
+        return (
+            <Square 
+                value={this.state.squares[i]}
+                onClick = {() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
         <div>
